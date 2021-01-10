502a50b23613501938fa60ab0728c99c87905316
kevinburke16 <45501524+kevinburke16@users.noreply.github.com>
2021-01-10 01:20:55 -0500

    Update config.js
    hidden key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'd8315dc1-4786-425a-83bf-0000204c4439';
+export const KEY = '<my key>';
 export const MODAL_CLOSE_SEC = 2.5;
